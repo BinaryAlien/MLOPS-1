0a2a3e7d2be982ddeee8747b1f91afd4293d9e4d
Binary <49949198+BinaryAlien@users.noreply.github.com>
2024-10-23 18:46:38 +0200

    Level 4

--- backend.py
+++ backend.py
@@ -1,9 +1,12 @@
 from flask import Flask, request
+from transformers import pipeline
 import joblib
 
 
 app = Flask(__name__)
-model = joblib.load("regression.joblib")
+
+regression = joblib.load("regression.joblib")
+camembert_fill_mask = pipeline("fill-mask", model="camembert-base")
 
 
 @app.route("/predict", methods=["POST"])
@@ -13,5 +16,10 @@ def predict():
         float(request.json["nb_rooms"]),
         int(request.json["garden"]),
     ]
-    output = model.predict([sample])
+    output = regression.predict([sample])
     return {"price": output[0]}
+
+
+@app.route("/camembert", methods=["POST"])
+def camembert():
+    return camembert_fill_mask(request.json["input"])
